659d40dc2720c74321d1c5d2b20840634c7b2a6c
Alice-Githui <gnyokabi.alice@gmail.com>
2021-04-01 21:39:09 +0300

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutlayComponent } from './outlay/outlay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutlayComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  APIKey:  "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"
+
 };
+
+//API KEY:  "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"
+//API KEY URL: 'https://api.github.com/users/daneden?access_token=' + apiKey
 
 /*
  * For easier debugging in development mode, you can import the following file
